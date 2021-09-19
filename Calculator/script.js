9114f529ad9e631547d6f95945894782ade4dc49
BenTzion <87093783+benntx@users.noreply.github.com>
2021-09-19 15:03:17 +0300

    JS UPDATE

--- Calculator/script.js
+++ Calculator/script.js
@@ -55,93 +55,255 @@ nums[3].addEventListener('click',(event) => // button number 7
 })
 nums[4].addEventListener('click',(event) => // button number 8
 {
-    if(screen.innerText == "")
+    if(screen.innerText == "0")
     {
         screen.innerText = 8;
         sum = 8;
     }
-    else
+    else if(screen.innerText.charAt(screen.innerText.length - 1) == calcOp[0].innerText)
+    {
         screen.innerText += " " + 8;
+        sum /= 8;
+    }
+    else if(screen.innerText.charAt(screen.innerText.length - 1) == calcOp[1].innerText)
+    {
+        screen.innerText += " " + 8;
+        sum *= 8;
+    }
+    else if(screen.innerText.charAt(screen.innerText.length - 1) == calcOp[2].innerText)
+    {
+        screen.innerText += " " + 8;
+        sum -= 8;
+    }
+    else
+    {
+        screen.innerText += " " + 8;
+        sum  += 8;
+    }
 })
 nums[5].addEventListener('click',(event) => // button number 9
 {
-    if(screen.innerText == "")
+    if(screen.innerText == "0")
     {
         screen.innerText = 9;
         sum = 9;
     }
-    else
+    else if(screen.innerText.charAt(screen.innerText.length - 1) == calcOp[0].innerText)
+    {
         screen.innerText += " " + 9;
+        sum /= 9;
+    }
+    else if(screen.innerText.charAt(screen.innerText.length - 1) == calcOp[1].innerText)
+    {
+        screen.innerText += " " + 9;
+        sum *= 9;
+    }
+    else if(screen.innerText.charAt(screen.innerText.length - 1) == calcOp[2].innerText)
+    {
+        screen.innerText += " " + 9;
+        sum -= 9;
+    }
+    else
+    {
+        screen.innerText += " " + 9;
+        sum  += 9;
+    }
 })
 nums[6].addEventListener('click',(event) => // button number 4
 {
-    if(screen.innerText == "")
+    if(screen.innerText == "0")
     {
         screen.innerText = 4;
         sum = 4;
     }
-    else
+    else if(screen.innerText.charAt(screen.innerText.length - 1) == calcOp[0].innerText)
+    {
         screen.innerText += " " + 4;
+        sum /= 4;
+    }
+    else if(screen.innerText.charAt(screen.innerText.length - 1) == calcOp[1].innerText)
+    {
+        screen.innerText += " " + 4;
+        sum *= 4;
+    }
+    else if(screen.innerText.charAt(screen.innerText.length - 1) == calcOp[2].innerText)
+    {
+        screen.innerText += " " + 4;
+        sum -= 4;
+    }
+    else
+    {
+        screen.innerText += " " + 4;
+        sum  += 4;
+    }
 })
 nums[7].addEventListener('click',(event) => // button number 5
 {
-    if(screen.innerText == "")
+    if(screen.innerText == "0")
     {
         screen.innerText = 5;
         sum = 5;
     }
-    else
+    else if(screen.innerText.charAt(screen.innerText.length - 1) == calcOp[0].innerText)
+    {
         screen.innerText += " " + 5;
+        sum /= 5;
+    }
+    else if(screen.innerText.charAt(screen.innerText.length - 1) == calcOp[1].innerText)
+    {
+        screen.innerText += " " + 5;
+        sum *= 5;
+    }
+    else if(screen.innerText.charAt(screen.innerText.length - 1) == calcOp[2].innerText)
+    {
+        screen.innerText += " " + 5;
+        sum -= 5;
+    }
+    else
+    {
+        screen.innerText += " " + 5;
+        sum  += 5;
+    }
 })
 nums[8].addEventListener('click',(event) => // button number 6
 {
-    if(screen.innerText == "")
+    if(screen.innerText == "0")
     {
         screen.innerText = 6;
         sum = 6;
     }
-    else
+    else if(screen.innerText.charAt(screen.innerText.length - 1) == calcOp[0].innerText)
+    {
         screen.innerText += " " + 6;
+        sum /= 6;
+    }
+    else if(screen.innerText.charAt(screen.innerText.length - 1) == calcOp[1].innerText)
+    {
+        screen.innerText += " " + 6;
+        sum *= 6;
+    }
+    else if(screen.innerText.charAt(screen.innerText.length - 1) == calcOp[2].innerText)
+    {
+        screen.innerText += " " + 6;
+        sum -= 6;
+    }
+    else
+    {
+        screen.innerText += " " + 6;
+        sum  += 6;
+    }
 })
 nums[9].addEventListener('click',(event) => // button number 1
 {
-    if(screen.innerText == "")
+    if(screen.innerText == "0")
     {
         screen.innerText = 1;
         sum = 1;
     }
-    else
+    else if(screen.innerText.charAt(screen.innerText.length - 1) == calcOp[0].innerText)
+    {
         screen.innerText += " " + 1;
+        sum /= 1;
+    }
+    else if(screen.innerText.charAt(screen.innerText.length - 1) == calcOp[1].innerText)
+    {
+        screen.innerText += " " + 1;
+        sum *= 1;
+    }
+    else if(screen.innerText.charAt(screen.innerText.length - 1) == calcOp[2].innerText)
+    {
+        screen.innerText += " " + 1;
+        sum -= 1;
+    }
+    else
+    {
+        screen.innerText += " " + 1;
+        sum  += 1;
+    }
 })
 nums[10].addEventListener('click',(event) => // button number 2
 {
-    if(screen.innerText == "")
+    if(screen.innerText == "0")
     {
         screen.innerText = 2;
         sum = 2;
     }
-    else
+    else if(screen.innerText.charAt(screen.innerText.length - 1) == calcOp[0].innerText)
+    {
         screen.innerText += " " + 2;
+        sum /= 2;
+    }
+    else if(screen.innerText.charAt(screen.innerText.length - 1) == calcOp[1].innerText)
+    {
+        screen.innerText += " " + 2;
+        sum *= 2;
+    }
+    else if(screen.innerText.charAt(screen.innerText.length - 1) == calcOp[2].innerText)
+    {
+        screen.innerText += " " + 2;
+        sum -= 2;
+    }
+    else
+    {
+        screen.innerText += " " + 2;
+        sum  += 2;
+    }
 })
 nums[11].addEventListener('click',(event) => // button number 3
 {
-    if(screen.innerText == "")
+    if(screen.innerText == "0")
     {
         screen.innerText = 3;
         sum = 3;
     }
-    else
+    else if(screen.innerText.charAt(screen.innerText.length - 1) == calcOp[0].innerText)
+    {
         screen.innerText += " " + 3;
+        sum /= 3;
+    }
+    else if(screen.innerText.charAt(screen.innerText.length - 1) == calcOp[1].innerText)
+    {
+        screen.innerText += " " + 3;
+        sum *= 3;
+    }
+    else if(screen.innerText.charAt(screen.innerText.length - 1) == calcOp[2].innerText)
+    {
+        screen.innerText += " " + 3;
+        sum -= 3;
+    }
+    else
+    {
+        screen.innerText += " " + 3;
+        sum  += 3;
+    }
 })
 nums[12].addEventListener('click',(event) => // button number 0
 {
-    if(screen.innerText == "")
+    if(screen.innerText == "0")
     {
         screen.innerText = 0;
         sum = 0;
     }
-    else
+    // else if(screen.innerText.charAt(screen.innerText.length - 1) == calcOp[0].innerText) // חילוק ב0 בעיה.
+    // {
+    //     screen.innerText += " " + 7; 
+    //     sum /= 7;
+    // }
+    else if(screen.innerText.charAt(screen.innerText.length - 1) == calcOp[1].innerText)
+    {
         screen.innerText += " " + 0;
+        sum *= 0;
+    }
+    else if(screen.innerText.charAt(screen.innerText.length - 1) == calcOp[2].innerText)
+    {
+        screen.innerText += " " + 0;
+        sum -= 0;
+    }
+    else
+    {
+        screen.innerText += " " + 0;
+        sum  += 0;
+    }
 })
 
 calcOp[0].addEventListener('click',(event) => // ÷
@@ -151,9 +313,6 @@ calcOp[0].addEventListener('click',(event) => // ÷
 calcOp[1].addEventListener('click',(event) => // *
 {
     screen.innerText += " " + calcOp[1].innerText;
-    console.log(screen.innerText.length)
-    screen.innerText = screen.innerText.padEnd(screen.innerText.length," ");
-    console.log(screen.innerText.length)
 })
 calcOp[2].addEventListener('click',(event) => // -
 {
