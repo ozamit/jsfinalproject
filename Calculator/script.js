fe9f0d35bd4cb4575d6db4c1de3788352580f80b
ozamit <87121069+ozamit@users.noreply.github.com>
2021-09-28 20:56:13 +0300

    Update script.js
    * שימוש ב querySelector כדי להאזין לכפתורים ובכך לקצר משמעותית  את הקוד, צריך עדיין להרחיב את זה לעוד כמה כפתורים
    * פונקציה שמדפיסה לקונסולה את כל המשתנים עם הערכים כדי לדעת בכל שלב מה הם "מחזיקים", עוזר בבקרה, אפשר למחוק לפני ההגשה
    * בניתי מסך נוסף עליון עם פונט מוקטן להצגת התרגיל ומסך נפרד (הקיים) לתוצאה ולמספרים שמקלידים עכשיו, ראיתי את זה במחשבונים שונים באינטרנט וזו נראית לי חוויה טובה
    * הכנתי כבר את הפונקצויאליות שמדפיסה כל פעם למסך הנכון את מה שצריך
    * הרבה מהדברים שהיו לנו וכתוצאה משינויים שלי כבר אין בהם שימוש השארתי ב comments, אחרי שנראה שהכל באמת תקין ובסדר אפשר למחוק

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,9 +1,36 @@
 let nums = document.getElementsByClassName('num');
 let calcOp = document.getElementsByClassName('calcOp');
 let screen = document.getElementById('screen');
+let upper_screen = document.getElementById('upper-screen');
 let sum = 0, lstNumInt = 0 , sumHezka = 0;
 let lstNum = "" , op = "" , firstOp = "";
 
+
+
+// דרך מקוצרת לקלוט לחיצה על כפתור במחשבון ולעשות איתה פעולה
+let num2 = document.querySelectorAll('.num')
+num2.forEach(label => {
+    label.addEventListener( 'click', (event) => {
+        console.log(label.innerText) // לצורך בקרה בלבד, אפשר למחוק בהמשך
+        funSum(label.innerText);
+    })
+})
+
+let op2 = document.querySelectorAll('.op_btn')
+op2.forEach(label => {
+    label.addEventListener( 'click', (event) => {
+        op = label.innerText;
+        firstOp = op;
+        screen.innerText = "";
+        upper_screen.innerText += sum + ' ' + label.innerText;
+        console.log(label.innerText); // לצורך בקרה בלבד, אפשר למחוק בהמשך
+        conLog(); // לצורך בקרה בלבד, אפשר למחוק בהמשך
+    })
+})
+
+
+/*
+// ** user click on number **
 nums[3].addEventListener('click',(event) => // button number 7
 {
     funSum(7);
@@ -53,18 +80,93 @@ nums[12].addEventListener('click',(event) => // button number 0
 {
     funSum(0);
 });
-function funAct()
+
+*/
+
+// ** user click on operator **
+nums[0].addEventListener('click',(event) => // Clear button
 {
-    if(op == calcOp[0].innerText)
+    upper_screen.innerText = '';
+    screen.innerText = 0;
+    sum = 0 , lstNumInt = 0 , sumHezka = 0;
+    op = "" , lstNum = "" , firstOp = "";
+    conLog();
+});
+
+nums[1].addEventListener('click',(event) => // %
+{
+    upper_screen.innerText += sum + '%';
+    screen.innerText = "";
+    op = calcOp[1].innerText;
+    firstOp = op;
+    conLog();
+});
+
+nums[2].addEventListener('click',(event) => // ²
+{
+    upper_screen.innerText += sum + '²';
+    screen.innerText = "";
+    op = calcOp[2].innerText;
+    firstOp = op;
+    conLog();
+});
+
+/*
+calcOp[0].addEventListener('click',(event) => // ÷
+{
+    upper_screen.innerText += sum + ' ÷';
+    screen.innerText = "";
+    op = calcOp[0].innerText;
+    firstOp = op;
+    conLog();
+});
+calcOp[1].addEventListener('click',(event) => // *
+{
+    upper_screen.innerText += sum + ' X';
+    screen.innerText = "";
+    op = calcOp[1].innerText;
+    firstOp = op;
+    conLog();   
+});
+calcOp[2].addEventListener('click',(event) => // -
+{
+    upper_screen.innerText += sum + ' -';
+    screen.innerText = "";
+    op = calcOp[2].innerText;
+    firstOp = op;
+    conLog();
+});
+calcOp[3].addEventListener('click',(event) => // +
+{
+    upper_screen.innerText += sum + ' +';
+    screen.innerText = "";
+    op = '+';
+    firstOp = op;
+    conLog();
+});
+*/
+
+calcOp[0].addEventListener('click',(event) => // =
+{
+    screen.innerText = funAct();
+    lstNumInt = 0 , sumHezka = 0;
+    op = "" , lstNum = "" , firstOp = "";
+});
+
+
+// בפעולות שקורות בלחיצה על שווה
+function funAct()
+{   upper_screen.innerText = '';
+    if(op == '/') // מבצע את פעולת חילוק
         if(lstNumInt == 0)
             return "Can't divide by 0"
         else
             sum /= lstNumInt;
-    else if(op == calcOp[1].innerText)
+    else if(op == '*') // מבצע את פעולת כפל
             sum *= lstNumInt;
-    else if(op == calcOp[2].innerText)
+    else if(op == '-') // מבצע את פעולת חיסור
             sum -= lstNumInt;
-    else if(op == calcOp[3].innerText)
+    else if(op == '+')  // מבצע את פעולת חיבור
             sum += lstNumInt;
     else if(op == nums[2].innerText) // מבצע את פעולת חזקה
             if(lstNum == "")
@@ -96,12 +198,18 @@ function funAct()
             else if(firstOp == calcOp[2].innerText)
                 sum = sum-lstNumInt/100;
             else if(firstOp == calcOp[3].innerText)
-                sum = sum+lstNumInt/100;        
+                sum = sum+lstNumInt/100;
     }
     return sum;
+    screen.innerText = sum;
+    
+    conLog();
 }
 
-function funSum(lastClick) // מבצע סיכום כל פעם איזה מספר רצה המשתמש ליצור בין לפני ובין אחרי האופרטור
+
+// הפעולה מתבצעת בלחיצה על ספרה
+// מבצע סיכום כל פעם איזה מספר רצה המשתמש ליצור בין לפני ובין אחרי האופרטור
+function funSum(lastClick) 
 {
     if(screen.innerText == "0")
         screen.innerText = "";
@@ -109,61 +217,24 @@ function funSum(lastClick) // מבצע סיכום כל פעם איזה מספר 
     {
         screen.innerText += lastClick;
         sum = parseInt(screen.innerText);
+        conLog()
     }
     else
     {
         screen.innerText += lastClick;
         lstNum += screen.innerText.charAt(screen.innerText.length-1);
         lstNumInt = parseInt(lstNum);
+        conLog()
     }
 }
 
-nums[0].addEventListener('click',(event) => // Clear button
-{
-    screen.innerText = 0;
-    sum = 0 , lstNumInt = 0 , sumHezka = 0;
-    op = "" , lstNum = "" , firstOp = "";
-});
-
-nums[1].addEventListener('click',(event) => // %
-{
-    screen.innerText += nums[1].innerText;
-    op = nums[1].innerText;
-});
-
-nums[2].addEventListener('click',(event) => // ²
-{
-    screen.innerText += nums[2].innerText.charAt(1);
-    op = nums[2].innerText;
-});
-
-calcOp[0].addEventListener('click',(event) => // ÷
-{
-    screen.innerText += calcOp[0].innerText;
-    op = calcOp[0].innerText;
-    firstOp = op;
-});
-calcOp[1].addEventListener('click',(event) => // *
-{
-    screen.innerText += calcOp[1].innerText;
-    op = calcOp[1].innerText;
-    firstOp = op; 
-});
-calcOp[2].addEventListener('click',(event) => // -
-{
-    screen.innerText += calcOp[2].innerText;
-    op = calcOp[2].innerText;
-    firstOp = op;
-});
-calcOp[3].addEventListener('click',(event) => // +
-{
-    screen.innerText += calcOp[3].innerText;
-    op = calcOp[3].innerText;
-    firstOp = op;
-});
-calcOp[4].addEventListener('click',(event) => // =
-{
-    screen.innerText = funAct();
-    lstNumInt = 0 , sumHezka = 0;
-    op = "" , lstNum = "" , firstOp = "";
-});
+function conLog(){
+    console.log(
+        "sum = " + sum + " | " +
+        "lstNumInt = " + lstNumInt + " | " +
+        "sumHezka = " + sumHezka + " | " +
+        "lstNum = " + lstNum + " | " +
+        "op = " + op + " | " +
+        "firstOp = " + firstOp + " | "
+        );
+};
