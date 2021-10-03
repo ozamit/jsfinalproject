7994a7c6124278ebcdc4108fdf518f4e84a79a77
BenTzion <87093783+benntx@users.noreply.github.com>
2021-10-03 03:35:28 +0300

    סיום המחשבון המבצע את כל הפעולות ובנוסף מכניס כל תרגיל לתוך מערך

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,130 +1,157 @@
 let nums = document.querySelectorAll('.num');
 let calcOp = document.querySelectorAll('.calcOp')
 let screen = document.getElementById('screen');
-let upperScreen = document.getElementById('upper-screen');
 let sum = 0, lstNumInt = 0 , sumHezka = 0;
-let lstNum = "" , op = "" , firstOp = "" , targil = "";
+let lstNum = "" , op = "" , opHez = "" , opAhoz = "";
+let num1Hez = false , num2Hez = false;
 let sumAll = [];
 
 nums.forEach(label =>
 {
-    if((label.innerText != 'C') && (label.innerText != '%') && (label.innerText != 'X²'))
+    if((label.innerText != 'C') && (label.innerText != '%') && (label.innerText != 'X²')) // Numbers Button
         label.addEventListener('click', (event) =>
         {
             funSum(label.innerText);
-            targil += screen.innerText;
         })
     else if(label.innerText == 'C')
         label.addEventListener('click',(event) => // Clear button
         {
             screen.innerText = 0 , sum = 0 , lstNumInt = 0 , sumHezka = 0;
-            op = "" , lstNum = "" , firstOp = "", upperScreen.innerText = "" , targil = "";
+            op = "" , lstNum = "" , opHez = "" , opAhoz = "";
+            num1Hez = false , num2Hez = false;
         });
     else if(label.innerText == '%')
-        label.addEventListener('click',(event) => // %
+        label.addEventListener('click',(event) => // % button
         {
             screen.innerText += label.innerText;
-            op = label.innerText;
+            opAhoz = label.innerText;
         });
     else
-        label.addEventListener('click',(event) => // ²
+        label.addEventListener('click',(event) => // ² button
         {
             screen.innerText += label.innerText.charAt(1);
-            op = label.innerText;
+            opHez = label.innerText;
+            if(lstNum == "")
+                num1Hez = true;
+            if(lstNum != "")
+                num2Hez = true;
         });
 })
 
 calcOp.forEach(label => 
-    {
-    if(label.innerText == "=")
-    {
-        label.addEventListener( 'click', (event) =>
+{
+    if(label.innerText == "=") // equal button
+        label.addEventListener('click', (event) =>
         {
-            screen.innerText = funAct();
-            targil += "=" + sum;
-            sumAll.push(targil);
-            targil = sum;
-            upperScreen.innerText = "";
+            screen.innerText += "=" + funAct();
+            sumAll.push(screen.innerText);
             lstNumInt = 0 , sumHezka = 0;
-            op = "" , lstNum = "" , firstOp = "";
-            console.log(targil);
+            op = "" , lstNum = "" , opHez = "" , opAhoz = "";
+            num1Hez = false , num2Hez = false;
             console.log(sumAll);
         });
-    }
-    else
-        label.addEventListener( 'click', (event) => 
+    else // other operators buttons
+        label.addEventListener('click', (event) => 
         {
-            screen.innerText = funAct();
             screen.innerText += label.innerText;
             op = label.innerText;
-            firstOp = op;
-            upperScreen.innerText = screen.innerText;
-            screen.innerText = "";
-            targil += op;
         });
-    });
+});
 
 function funAct() // מבצע את תהליך האופרטור הלחוץ
 {
-    if(op != "")
-    {
-        if(op == "/")
-            if(lstNumInt == 0)
-                return "Can't divide by 0"
+    if(op == "/" && opHez == "" && opAhoz == "") // מבצע פעולת חילוק
+        if(lstNumInt == 0)
+            return "Can't divide by 0"
+        else
+            sum /= lstNumInt;
+    else if(op == "*" && opHez == "" && opAhoz == "") // מבצע פעולת כפל
+            sum *= lstNumInt;
+    else if(op == "-" && opHez == "" && opAhoz == "") // מבצע פעולת חיסור
+            sum -= lstNumInt;
+    else if(op == '+' && opHez == "" && opAhoz == "") // מבצע פעולת חיבור
+            sum += lstNumInt;
+    else if(opHez == 'X²') // מבצע את פעולת החזקה
+            if(lstNum == "") // בודק אם משתמש רוצה לעשות חזקה רק על המספר הראשון בלבד
+                sum = sum*sum;
             else
-                sum /= lstNumInt;
-        else if(op == "*")
-                sum *= lstNumInt;
-        else if(op == "-")
-                sum -= lstNumInt;
-        else if(op == '+')
-                sum += lstNumInt;
-        else if(op == 'X²') // מבצע את פעולת חזקה
-                if(lstNum == "")
-                    sum = sum*sum;
-                else
+                {
+                    if(num1Hez == true && num2Hez == true) // בודק אם נלחץ חזקה בשני המספרים
                     {
-                        if(firstOp == "/")
+                        if(op == "/")
                             if(lstNumInt == 0)
                                 return "Can't divide by 0";
                             else
-                                sumHezka = lstNumInt*lstNumInt / sum;
-                        else if(firstOp == "*")
-                                sumHezka = lstNumInt*lstNumInt * sum;
-                        else if(firstOp == "-")
-                                sumHezka = lstNumInt*lstNumInt - sum;
-                        else if(firstOp == "+")
-                                sumHezka = lstNumInt*lstNumInt + sum;
-                        sum = sumHezka;
+                                sumHezka = (sum*sum) / (lstNumInt*lstNumInt);
+                        else if(op == "*")
+                                sumHezka = (sum*sum) * (lstNumInt*lstNumInt);
+                        else if(op == "-")
+                                sumHezka = (sum*sum) - lstNumInt*lstNumInt;
+                        else if(op == "+")
+                                sumHezka = (sum*sum) + (lstNumInt*lstNumInt);
                     }
-        else // מבצע את פעולת האחוז
-        {
-                if(firstOp == "/")
-                    if(lstNumInt == 0)
-                        return "Can't divide by 0";
-                    else
-                        sum = sum/lstNumInt/100;
-                else if(firstOp == "*")
-                    sum = sum*lstNumInt/100;
-                else if(firstOp == "-")
-                    sum = sum-lstNumInt/100;
-                else if(firstOp == "+")
-                    sum = sum+lstNumInt/100;        
-        }
+                    else if(num1Hez == true) // בודק אם נלחץ חזקה רק במספר הראשון
+                    {
+                        if(op == "/")
+                            if(lstNumInt == 0)
+                                return "Can't divide by 0";
+                            else
+                                sumHezka = sum*sum / lstNumInt;
+                        else if(op == "*")
+                            sumHezka = sum*sum * lstNumInt;
+                        else if(op == "-")
+                            sumHezka = sum*sum - lstNumInt;
+                        else if(op == "+")
+                            sumHezka = sum*sum + lstNumInt;
+                    }
+                    else // בודק אם נלחץ חזקה רק במספר השני
+                    {
+                        if(op == "/")
+                            if(lstNumInt == 0)
+                                return "Can't divide by 0";
+                            else
+                                sumHezka =  sum / (lstNumInt*lstNumInt);
+                        else if(op == "*")
+                            sumHezka = sum*lstNumInt*lstNumInt;
+                        else if(op == "-")
+                            sumHezka = sum - lstNumInt*lstNumInt;
+                        else if(op == "+")
+                            sumHezka = sum + lstNumInt*lstNumInt;               
+                    }
+                    sum = sumHezka;
+                }
+    else // מבצע את פעולת האחוז
+    {
+        if(op == "/")
+            if(lstNumInt == 0)
+                    return "Can't divide by 0";
+                else
+                    sum = sum/lstNumInt/100;
+            else if(op == "*")
+                sum = sum*lstNumInt/100;
+            else if(op == "-")
+                sum = sum-lstNumInt/100;
+            else if(op == "+")
+                sum = sum+lstNumInt/100;        
     }   
     return sum;
 }
 
 function funSum(lastClick) // מבצע סיכום כל פעם איזה מספר רצה המשתמש ליצור בין לפני ובין אחרי האופרטור
 {
-    if(screen.innerText == "0")
+    if(screen.innerText == "0") // אצחול הצג בכדי שספרת 0 לא תשורשר
         screen.innerText = "";
-    if(op == "")
+    if(sumAll[sumAll.length-1] == screen.innerText) // מבצע אתחול מחדש של המסך
+    {
+        screen.innerText = lastClick;
+        sum = parseInt(screen.innerText);
+    }
+    else if(op == "") // יוצר את המספר שלפני האופרטור
     {
         screen.innerText += lastClick;
         sum = parseInt(screen.innerText);
     }
-    else
+    else // יוצר את המספר שלאחר האופרטור
     {
         screen.innerText += lastClick;
         lstNum += screen.innerText.charAt(screen.innerText.length-1);
