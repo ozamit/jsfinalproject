ce6dc638d750600cc175efa86dc1c38e176eb59b
ozamit <87121069+ozamit@users.noreply.github.com>
2021-10-10 20:12:34 +0300

    Update script.js

--- LogIn/script.js
+++ LogIn/script.js
@@ -4,13 +4,20 @@ let email_field = document.getElementById("eMail");
 let email_value = "";
 let submitBtn = document.getElementById("submit")
 
+
 submitBtn.addEventListener ("click", (event) => {
-    full_name_value = full_name_field.value;
-    email_value = email_field.value;
-    console.log(full_name_value + " - " + email_value);
-    window.location.href = "file:///C:/fullstack/fullstackcourse/jsfinalproject/Calculator/calculator.html" + "?name=" + full_name_value + "&email=" + email_value;
+
+    if (full_name_field.value == "" && email_field.value == "") {
+        alert("Please fill the form")
+    } else if (full_name_field.value == "") {
+        alert("please enter name")
+    } else if (email_field.value == "") {
+        alert("please enter email")
+    } else if (email_field.value.includes("@") == false) {
+        alert("please enter valid email (with @)") 
+    } else {
+        full_name_value = full_name_field.value;
+        email_value = email_field.value;
+        window.location.href = "file:///C:/fullstack/fullstackcourse/jsfinalproject/Calculator/calculator.html" + "?name=" + full_name_value + "&email=" + email_value;
+    }
 })
-
-
-
-
